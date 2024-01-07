71d98d39cc930ab56f9396182108e1ae381078a4
karthik617 <yadavkarthik617@gmail.com>
2024-01-07 21:02:48 +0530

    disney sidebar copy

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import Image from "next/image"
 import logo from "@/public/One-Piece-Logo.png"
 import { useEffect,useState } from "react"
+import { usePathname } from "next/navigation"
 
 interface SideBarMenuType {
     id: number,
@@ -12,6 +13,7 @@ interface SideBarMenuType {
     link: string
 }
 const Sidebar = () => {
+    const path = usePathname()
     const [expanded, setExpanded] = useState(false);
     const handleMouseOver = () => {
         setExpanded(true);
@@ -26,7 +28,7 @@ const Sidebar = () => {
             <>
             {SideBarMenu.map((item: SideBarMenuType) => (
             <Link key={item.id} href={item.link}>
-                <div className="flex gap-1 justify-center items-center cursor-pointer text-gray-400 hover:scale-110 hover:translate-x-[7px] hover:text-white hover:shadow-lg">
+                <div className={`flex gap-1 justify-center items-center cursor-pointer ${path.includes(item.title.toLowerCase()) ?"text-white" : "text-gray-400"} hover:scale-110 hover:translate-x-[7px] hover:text-white hover:shadow-lg`}>
                     <div>{item.icon}</div>
                     <div className={`text-2xl ${!expanded ? "hidden" : ""}`}>{item.title}</div>
                 </div>
